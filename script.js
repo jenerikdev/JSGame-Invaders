e92222f98356fe0467e3e2167cec16bab94a2403
jenerikdev <jenerikmedic@gmail.com>
2024-10-04 14:19:41 -0500

    First commit of this workspace. Remake of Space Invaders with updated enemy's from Frank'sLaboratory on youtube.

---
+++ script.js
@@ -0,0 +1,42 @@
+class Player{
+    constructor(game){
+        this.game = game;
+        this.width = 100;
+        this.height = 100;
+        this.x = this.game.width * 0.5 - this.width * 0.5;
+        this.y = this.game.height - this.height;
+    }
+    draw(context){
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Projectile{
+
+}
+
+class Enemy{
+
+}
+
+class Game{
+    constructor(canvas){
+        this.canvas =canvas;
+        this.width =canvas.width;
+        this.height = canvas.height;
+        this.player = new Player(this);
+    }
+    render(context){
+        this.player.draw(context);
+    }
+}
+
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx =  canvas.getContext('2d');
+    canvas.width = 600;
+    canvas.height = 800;
+
+    const game = new Game(canvas);
+    game.render(ctx);
+})
