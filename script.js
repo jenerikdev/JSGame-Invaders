c1505399d1ad8ab5ec6285685d91abb9890fa297
jenerikdev <130342129+jenerikdev@users.noreply.github.com>
2024-10-05 23:01:35 -0500

    Added eventListeners for controlling with arrow keys

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ class Player{
         this.height = 100;
         this.x = this.game.width * 0.5 - this.width * 0.5;
         this.y = this.game.height - this.height;
+        this.speed = 10;
     }
     draw(context){
         context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    update(){
+        if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
+        if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
     }
 }
 
@@ -24,10 +29,21 @@ class Game{
         this.canvas =canvas;
         this.width =canvas.width;
         this.height = canvas.height;
+        this.keys = [];
         this.player = new Player(this);
+
+        window.addEventListener('keydown', e => {
+            if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+        });
+        window.addEventListener('keyup', e => {
+            const index = this.keys.indexOf(e.key);
+            if (index > -1) this.keys.splice(index, 1);
+        });
+
     }
     render(context){
         this.player.draw(context);
+        this.player.update();
     }
 }
 
@@ -38,5 +54,11 @@ window.addEventListener('load', function(){
     canvas.height = 800;
 
     const game = new Game(canvas);
-    game.render(ctx);
-})
+
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
